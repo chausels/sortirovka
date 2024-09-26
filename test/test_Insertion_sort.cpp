dc1022c542b7f1343946db8b90453ef248c80b55
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 21:08:43 +0300

    Create test_Insertion_sort.cpp

---
+++ test/test_Insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include <catch.hpp>
+#include "insertion_sort.h"
+
+TEST(InsertionSortTest, EmptyArray) {
+    int arr[] = {};
+    insertionSort(arr, 0);
+    ASSERT_EQ(sizeof(arr) / sizeof(arr[0]), 0);
+}
+
+TEST(InsertionSortTest, SingleElement) {
+    int arr[] = {5};
+    insertionSort(arr, 1);
+    ASSERT_EQ(arr[0], 5);
+}
+
+TEST(InsertionSortTest, SortedArray) {
+    int arr[] = {1, 2, 3, 4, 5};
+    insertionSort(arr, 5);
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_EQ(arr[i], i + 1);
+    }
+}
+
+TEST(InsertionSortTest, ReverseArray) {
+    int arr[] = {5, 4, 3, 2, 1};
+    insertionSort(arr, 5);
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_EQ(arr[i], i + 1);
+    }
+}
