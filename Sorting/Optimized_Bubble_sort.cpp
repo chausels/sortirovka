432ab97b5c604c9790e5e9ebfee65bcaf5eee62d
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 19:49:47 +0300

    Create Optimized_Bubble_sort.cpp

---
+++ Sorting/Optimized_Bubble_sort.cpp
@@ -0,0 +1,17 @@
+template <typename T>
+void optimizedBubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
