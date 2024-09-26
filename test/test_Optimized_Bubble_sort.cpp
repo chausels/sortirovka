06acd1a0f31c2575f7f7d682ad0ac185c08eb522
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 21:24:38 +0300

    Update test_Optimized_Bubble_sort.cpp

--- test/test_Optimized_Bubble_sort.cpp
+++ test/test_Optimized_Bubble_sort.cpp
@@ -1,19 +1,19 @@
 #include <catch.cpp>
 #include "Optimized_Bubble_sort.h"
 
-TEST(OptimizedBubbleSortTest, HandlesEmptyArray) {
+TEST(OptimizedBubbleSortTest, EmptyArray) {
     int arr[] = {};
     optimized_bubble_sort(arr, 0);
     ASSERT_EQ(arr[0], 0); // No elements to compare
 }
 
-TEST(OptimizedBubbleSortTest, HandlesSingleElement) {
+TEST(OptimizedBubbleSortTest, SingleElement) {
     int arr[] = {10};
     optimized_bubble_sort(arr, 1);
     ASSERT_EQ(arr[0], 10);
 }
 
-TEST(OptimizedBubbleSortTest, HandlesAlreadySortedArray) {
+TEST(OptimizedBubbleSortTest, SortedArray) {
     int arr[] = {1, 2, 3, 4, 5};
     optimized_bubble_sort(arr, 5);
     ASSERT_EQ(arr[0], 1);
