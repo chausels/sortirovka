425b7d25de4fd81c9e44dbdb3b46dbb9d8741602
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 21:26:11 +0300

    Create test_Selection_sort.cpp

---
+++ test/test_Selection_sort.cpp
@@ -0,0 +1,30 @@
+#include <catch.h>
+#include "Selection_sort.h"
+
+TEST(SelectionSortTest, EmptyArray) {
+    int arr[] = {};
+    selectionSort(arr, 0);
+    ASSERT_EQ(sizeof(arr) / sizeof(arr[0]), 0);
+}
+
+TEST(SelectionSortTest, SingleElement) {
+    int arr[] = {3};
+    selectionSort(arr, 1);
+    ASSERT_EQ(arr[0], 3);
+}
+
+TEST(SelectionSortTest, AlreadySortedArray) {
+    int arr[] = {1, 2, 3, 4, 5};
+    selectionSort(arr, 5);
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_EQ(arr[i], i + 1);
+    }
+}
+
+TEST(SelectionSortTest, ReverseSortedArray) {
+    int arr[] = {5, 4, 3, 2, 1};
+    selectionSort(arr, 5);
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_EQ(arr[i], i + 1);
+    }
+}
