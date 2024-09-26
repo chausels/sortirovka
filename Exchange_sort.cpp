0f6ebbc226c63873f4c537bdd70a45b5240821d9
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 18:56:38 +0300

    Create Exchange_sort.cpp

---
+++ Exchange_sort.cpp
@@ -0,0 +1,12 @@
+template <typename T>
+void exchangeSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
