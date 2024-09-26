2f87ef9b5fb7500421d712b2280bd273a0d7d98b
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 21:06:48 +0300

    Create test_Exchange_sort.cpp

---
+++ test/test_Exchange_sort.cpp
@@ -0,0 +1,22 @@
+#include <catch.hpp>
+#include "exchange_sort.h"
+
+TEST(ExchangeSortTest, EmptyArray) {
+    int arr[] = {};
+    exchangeSort(arr, 0);
+    ASSERT_EQ(sizeof(arr) / sizeof(arr[0]), 0);
+}
+
+TEST(ExchangeSortTest, SingleElement) {
+    int arr[] = {5};
+    exchangeSort(arr, 1);
+    ASSERT_EQ(arr[0], 5);
+}
+
+TEST(ExchangeSortTest, UnsortedArray) {
+    int arr[] = {4, 2, 3, 1, 5};
+    exchangeSort(arr, 5);
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_EQ(arr[i], i + 1);
+    }
+}
