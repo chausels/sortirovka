c0255b58e03de4de65714425f94a99e4e2415ff7
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 18:28:13 +0300

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,14 +1,12 @@
-#include "insertion_sort.h"
+#include "bubble_sort.h"
 
 template <typename T>
-void insertionSort(T arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        T key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+void bubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
         }
-        arr[j + 1] = key;
     }
 }
