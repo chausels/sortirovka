cdde1b609c0c8a60b4adde7b7dfb951d2ca0c0e5
chausels <malvina.mamedrzaeva@gmail.com>
2024-09-26 19:51:42 +0300

    Create Selection_sort.cpp

---
+++ Sorting/Selection_sort.cpp
@@ -0,0 +1,13 @@
+template <typename T>
+void selectionSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        T temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
